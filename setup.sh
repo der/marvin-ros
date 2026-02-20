9304fb196a8880a5871ff00d2d933b481eab0891
der <dave@epimorphics.com>
2026-02-20 21:59:20 +0000

    Add set up script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+. /opt/ros/jazzy/setup.sh
+. ./install/setup.sh
+# Ensure access to the virtual environment packages
+export PYTHONPATH="$PYTHONPATH:/home/der/projects/home/ros2/sound_test_ws/venv/lib/python3.12/site-packages"
